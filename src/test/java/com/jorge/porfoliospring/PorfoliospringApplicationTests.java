9e21dd7c48515b28643451d1e68fe2facc293d23
JorgeAsca <jascola171@g.educaand.es>
2025-03-06 03:17:36 -1100

    Agregando dependencias de entrada

---
+++ src/test/java/com/jorge/porfoliospring/PorfoliospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorge.porfoliospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorfoliospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
